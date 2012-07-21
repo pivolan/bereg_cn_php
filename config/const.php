bc2c8179e50be9e3d7938f8e68bc846d166a6328
PiVolan <pivolan@gmail.com>
2012-07-22 02:13:12 +0400

    изменен почтовый ящик для получения почты с формы обратной связи. на мой

--- config/const.php
+++ config/const.php
@@ -1,5 +1,5 @@
 <?php
-	$ADMIN_MAIL="pechenikinV@gmail.com";
+	$ADMIN_MAIL="beregcn@pivolan.ru";
 	$STRIP_TAGS="<a><p><img><span><div><i><u><b><strong><em><sup><sub><blockquote><table><tr><td><th><tbody>";
 	$PAGE_NUM_SUCC=2;
 	$PAGE_NUM_ERROR=3;
