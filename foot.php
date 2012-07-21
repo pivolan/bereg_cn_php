8ce019d1ae6d1b5effea91c1c95ca556679da471
PiVolan <pivolan@gmail.com>
2012-07-22 02:00:21 +0400

    удаление счетчиков

--- foot.php
+++ foot.php
@@ -16,38 +16,18 @@
 </div>
   <script type="text/javascript" src="pivo.js"></script>
   <div>
-  <!--LiveInternet counter--><script type="text/javascript"><!--
-document.write("<a href='http://www.liveinternet.ru/click' "+
-"target=_blank><img src='http://counter.yadro.ru/hit?t44.6;r"+
-escape(document.referrer)+((typeof(screen)=="undefined")?"":
-";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
-screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
-";h"+escape(document.title.substring(0,80))+";"+Math.random()+
-"' alt='' title='LiveInternet' "+
-"border='0' width='31' height='31'><\/a>")
-//--></script><!--/LiveInternet-->
 </div>
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
     <script type="text/javascript">
     $(function() {
         $('a.img').lightBox();
     });
     </script>
 
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-10303439-1");
-pageTracker._trackPageview();
-} catch(err) {}</script>
 <div style='position: absolute; top:-9999px;'>
 <img src='images/activeLink.png'/>
 <img src='images/linkrightActive.png'/>
 <img src='images/submenu.png'/>
 </div>
 <a title='<?=$a;?>' onclick='alert(this.title)'>информация</a>
-<img src="http://counter.pr-cy.ru/prcy/bereg-cn.ru" alt="PR-CY.ru" width="88" height="31" border="0" />
 </body>
 </html>
